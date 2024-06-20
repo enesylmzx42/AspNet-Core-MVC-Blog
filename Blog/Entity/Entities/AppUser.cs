4bed2fa75f1f04b004c3c24f9a199a9ad13b58e2
Enes Yılmaz <117593621+enesylmzx42@users.noreply.github.com>
2024-06-20 11:53:44 +0300

    Update AppUser.cs

--- Blog/Entity/Entities/AppUser.cs
+++ Blog/Entity/Entities/AppUser.cs
@@ -14,7 +14,7 @@ namespace Entity.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
-        public Guid? ImageId { get; set; } = Guid.Parse("4084c97a-2aa1-4675-b519-69f6fe410633");
+        public Guid? ImageId { get; set; }
         public Image Image { get; set; }
 
         public ICollection<Article> Articles { get; set; }
